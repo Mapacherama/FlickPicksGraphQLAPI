91b89197e0a456a94d1e2abd1c4eea1828fb6ce9
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-01 09:49:18 +0200

    Feature: The rest of the service implementation

--- FlickPicksGraphQLBackend/Services/IMovieService.cs
+++ FlickPicksGraphQLBackend/Services/IMovieService.cs
@@ -1,6 +1,15 @@
-﻿namespace FlickPicksGraphQLBackend.Services
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FlickPicksGraphQLBackend.Models;
+
+namespace FlickPicksGraphQLBackend.Services
 {
     public interface IMovieService
     {
+        Task<IEnumerable<Movie>> GetAllMovies();
+        Task<Movie> GetMovieById(int id);
+        Task<Movie> CreateMovie(Movie movie);
+        Task<Movie> UpdateMovie(int id, Movie updatedMovie);
+        Task<bool> DeleteMovie(int id);
     }
 }
